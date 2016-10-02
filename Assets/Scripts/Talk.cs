45db1c8eee69f50ca1920075de5e887d01265bc8
KAZE\harunakaze <haruna.kaze@gmail.com>
2016-10-03 00:47:19 +0700

    !* Only talk inside

--- Assets/Scripts/Talk.cs
+++ Assets/Scripts/Talk.cs
@@ -5,17 +5,21 @@ public class Talk : MonoBehaviour {
     public GameObject bubbleTalk;
     public Flowchart flowchart;
 
+    private bool isInside = false;
+
     void Update() {
-        if(Input.GetKeyDown(KeyCode.Space)) {
+        if(Input.GetKeyDown(KeyCode.Space) && isInside) {
             flowchart.ExecuteBlock("Oh wow");
         }
     }
 
     void OnTriggerEnter2D(Collider2D other) {
+        isInside = true;
         bubbleTalk.SetActive(true);
     }
 
     void OnTriggerExit2D(Collider2D other) {
+        isInside = false;
         bubbleTalk.SetActive(false);
     }
 }
