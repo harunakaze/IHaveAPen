d25d60c0bd5f877242afae2b399173c80e17a595
KAZE\harunakaze <haruna.kaze@gmail.com>
2016-10-03 00:04:02 +0700

    + Tower + Talk with space

---
+++ Assets/Scripts/Talk.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Fungus;
+
+public class Talk : MonoBehaviour {
+    public GameObject bubbleTalk;
+    public Flowchart flowchart;
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            flowchart.ExecuteBlock("Oh wow");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        bubbleTalk.SetActive(true);
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        bubbleTalk.SetActive(false);
+    }
+}
