e9a1fca45025438fed45426795f7fae527377ef3
KAZE\harunakaze <haruna.kaze@gmail.com>
2016-10-03 14:06:10 +0700

    + Picture scroll editor

---
+++ Assets/Scripts/Editor/PictureScrollEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomPropertyDrawer(typeof(PictureScroll))]
+public class PictureScrollEditor : PropertyDrawer {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        EditorGUI.BeginProperty(position, label, property);
+
+        EditorGUI.PropertyField(position, property.FindPropertyRelative("sprite"), label);
+
+        EditorGUI.EndProperty();
+    }
+}
